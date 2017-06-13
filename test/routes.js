0c6018272c2f4f42efb369d6e29f7808cc024c55
vemuez <vemuez@gmail.com>
2017-06-13 12:32:33 +0100

    uncommented it method

--- test/routes.js
+++ test/routes.js
@@ -6,18 +6,18 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Product Api Controllers', function () {
-    it('should get all products /api/products GET', function (done) {
-        // chai.request(server)
-        //     .get("/api/product")
-        //     .end(function (err, res) {
-        //         res.should.have.status(200);
-        //         res.should.be.json;
-        //         res.body.should.have.property('success');
-        //         res.body.success.should.be.a('boolean');
-        //         res.body.success.should.equal(true);
-        //         res.body.should.have.property('products');
-        //         res.body.products.should.be.a('array');
-        //         done();
-        //     });
-    });
+    // it('should get all products /api/products GET', function (done) {
+    //     chai.request(server)
+    //         .get("/api/product")
+    //         .end(function (err, res) {
+    //             res.should.have.status(200);
+    //             res.should.be.json;
+    //             res.body.should.have.property('success');
+    //             res.body.success.should.be.a('boolean');
+    //             res.body.success.should.equal(true);
+    //             res.body.should.have.property('products');
+    //             res.body.products.should.be.a('array');
+    //             done();
+    //         });
+    // });
 });
